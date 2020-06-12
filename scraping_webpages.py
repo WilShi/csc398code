539a43c9fc140ea9b0b5765ad488d38d39f52b66
WilShi <47671779+WilShi@users.noreply.github.com>
2020-06-12 00:32:38 -0400

    update scraping_webpages
    It can crawl all pages through the base URL and automatically crawl all previous conferences.

--- scraping_webpages.py
+++ scraping_webpages.py
@@ -1,11 +1,15 @@
 import sys
 import requests
 import re
+import nltk
 from bs4 import BeautifulSoup
+from nltk.corpus import wordnet
+from itertools import groupby
+from nltk.tree import Tree
 
 def make_soup(url):
     html = requests.get(url) #Creat html file by url
-    soup = BeautifulSoup(html.text, 'html.parser')
+    soup = BeautifulSoup(html.text, 'lxml')
     return soup
 
 def get_link(soup): #return all html code in navigation bar as a list
@@ -33,29 +37,187 @@ def get_speaker_page(links): #return speaker page link
             link = i.get('href')
             return link
 
-def get_info(url):
-    soup = make_soup(url)
-    div = soup.find('div')
-    while (div.find('h2') == None):
-        div = soup.find()
-    
-    a = soup.find_all('h2')
+def get_committee(links):
+    link = []
+    for i in links:
+        if (i.find(text=re.compile('[Cc]ommittee')) != None):
+            link.append(i.get('href'))
+    return link
 
-    return a
+def get_pages(links):
+    pages=[]
+    for i in links:
+        if (i.find(text=re.compile('[Pp]revious')) == None and\
+             i.find(text=re.compile('\d{4}')) == None):
+            link = i.get('href')
+            pages.append(link)
+    return pages
+
+def get_previous_years(links):
+    pages=[]
+    for i in links:
+        if (i.find(text=re.compile('[Pp]revious')) != None or\
+             i.find(text=re.compile('\d{4}')) != None):
+            link = i.get('href')
+            if (link[:link.find('.')] != 'index'):
+                pages.append(link)
+    return pages
+
+def get_names(url):
+    soup = make_soup(url)
+    if ('steering' in url):
+        # title = soup.find('h1')
+        # print('Title: ' + title.text + '\n')
+        body = soup.find_all('tr')
+        for i in body:
+            info = ' '.join(i.text.split())
+            print(info + '\n')
+        return None
+
+    if ('accepted' in url):
+        na = soup.find_all('p', class_='', style='')
+        for i in na:
+            # get_namesAndOrg(i.text)
+            info = i.text
+            # info = info.replace('and', ',')
+            info = info.replace('; ', '\n\n').lstrip()
+            info = info[:info.find('(')].replace(', ', info[info.find('(')-1:]\
+                + '\n\n') + info[info.find('('):]
+            print(info + '\n')
+            # print("===================================")
+        if (url[-3:] == 'php'):
+            # print("===================================")
+            brs = soup.find_all('div', class_='list-group-item')
+            for br in brs:
+                info = ' '.join(str(br).split())
+                tag = re.findall('<br/>.*</div>', info)[0]
+                tag = re.sub(r'<[^>]*>', '', tag).lstrip()
+                # tag = tag.replace(', ', '\n\n').lstrip()
+                if ('2019' not in url):
+                    tag = tag.replace('), ', ')\n\n').lstrip()
+                else:
+                    tag = tag.replace('; ', '\n\n').lstrip()
+                tag = tag[:tag.find('(')].replace(', ', tag[tag.find('(')-1:]\
+                     + '\n\n') + tag[tag.find('('):]
+                # tag = tag[:tag.find('(')].replace(', ', '\n\n') + tag[tag.find('('):]
+                print(tag + '\n')
+                # print("===================================")
+        return None
+    
+    if ('program.php' in url or '2016/committee-organizing' in url):
+        body = soup.select('tr')
+        ct=0
+        count=0
+        for b in body:
+            if ('Members' in b.text):
+                ct=1
+                # break
+            elif (ct == 0):
+                info = b.text.replace('\n', ' ').lstrip()
+                print(info + '\n')
+            else:
+                info = ' '.join(b.text.split())
+                if (info == ''):
+                    count=0
+                elif (count%2 == 0):
+                    print(info, end=', ')
+                    count+=1
+                else:
+                    print(info + '\n')
+                    count=0
+        return None
+
+    # title = soup.find('h1')
+    # print('Title: ' + title.text + '\n')
+    body = soup.select('td')
+    count=0
+    for i in body:
+        info = ' '.join(i.text.split())
+        if ('Chairs' in info or 'Members' in info or 'Chair' in info or 'Organizers' in info\
+            or 'Treasurer' in info):
+            print('Position: ' + info)
+            count=0
+        elif (info == ''):
+            count=0
+        elif (':' in info):
+            None
+        elif ('Workshop' in info or 'events' in info):
+            break
+        elif (count%2 == 0):
+            print(info, end=', ')
+            count+=1
+        else:
+            print(info + '\n')
+            count=0
+
+def get_namesAndOrg(text):
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentt = nltk.ne_chunk(pos, binary = False)
+
+    name_list = []
+
+    for i in sentt:
+        # print(type(i))
+        if (type(i) == tuple):
+            sentt.remove(i)
+    # print(sentt)
+
+    ne_in_sent = []
+    for subtree in sentt:
+        if type(subtree) == Tree: # If subtree is a noun chunk, i.e. NE != "O"
+            ne_label = subtree.label()
+            ne_string = " ".join([token for token, pos in subtree.leaves()])
+            ne_in_sent.append((ne_string, ne_label))
+    # print(ne_in_sent)
+    for chunk, tag in ne_in_sent:
+        if(tag == 'PERSON'):
+            name_list.append(chunk)
+
+    for chunk, tag in ne_in_sent:
+        if(tag == 'PERSON'):
+            print('\n')
+        print(chunk, end=', ')
+
+def crawl_site(links):
+    name = ['accepted', 'cfw', 'committee-steering', \
+        'committee-program', 'committee-organizing', 'workshops']
+
+    for link in links:
+        if (link[:link.find('.')] in name):
+            print(link)
+            print('************************************')
+            print('Names, Organization, Location' + '\n')
+            if (link == './workshops.html'):
+                get_names(url + 'workshops.html')
+                print()
+            else:
+                get_names(url + link)
+                print()
 
 if __name__ == '__main__':
-    url=sys.argv[1]
-
-    # html = requests.get(url) #Creat html file by url
+    url = "http://www.ieee-security.org/TC/EuroSP2020/"
     soup = make_soup(url)
+    links_html = get_link(soup)
 
-    links = get_link(soup)
+    links = get_pages(links_html)
 
-    speaker_page_url = get_speaker_page(links)
-    print(speaker_page_url)
+    print(re.findall('EuroSP\d{4}', url)[0])
+    print('************************************')
+    crawl_site(links)
 
-    hs = get_info(speaker_page_url)
-    for i in hs:
-        print(i.text)
+    # crawl previous years
+    previous_years = get_previous_years(links_html)
+    for url in previous_years:
+        soup = make_soup(url)
+        links_html = get_link(soup)
+        links = get_pages(links_html)
+        print(re.findall('EuroSP\d{4}', url)[0])
+        print('************************************')
+        crawl_site(links)
 
+    print('************************************')
+    for link in previous_years:
+        print(link)
+    print('************************************')
     
