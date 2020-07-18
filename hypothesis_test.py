b42f705459d65ad189aecd07bed4ec15760a3f14
WilShi <47671779+WilShi@users.noreply.github.com>
2020-07-18 01:38:52 -0400

    Add files via upload

--- hypothesis_test.py
+++ hypothesis_test.py
@@ -34,110 +34,110 @@ def hyp_test(size1, year1, size2, year2):
 
 def run_test():
 
-    print("Female proportion does not change in 2016 and 2020 in CCS")
-    hyp_test(1310, 231, 175, 33)
+    print("Female proportion does not change in 2016 and 2019 in CCS")
+    hyp_test(1310, 231, 1543, 313)
 
-    print("\nWhite participants proportion does not change in 2016 and 2020 in CCS")
-    hyp_test(1310, 462, 175, 56)
+    print("\nWhite participants proportion does not change in 2016 and 2019 in CCS")
+    hyp_test(1310, 462, 1543, 513)
 
-    print("\nHispano participants proportion does not change in 2016 and 2020 in CCS")
-    hyp_test(1310, 123, 175, 24)
+    print("\nHispano participants proportion does not change in 2016 and 2019 in CCS")
+    hyp_test(1310, 123, 1543, 149)
 
-    print("\nAsian participants proportion does not change in 2016 and 2020 in CCS")
-    hyp_test(1310, 648, 175, 82)
+    print("\nAsian participants proportion does not change in 2016 and 2019 in CCS")
+    hyp_test(1310, 648, 1543, 823)
 
-    print("\nBlack participants proportion does not change in 2016 and 2020 in CCS")
-    hyp_test(1310, 77, 175, 13)
-
-    print('\n', '*'*70)
-
-    print('\n')
-    print("Female proportion does not change in 2014 and 2020 in SP")
-    hyp_test(390, 67, 1465, 274)
-
-    print("\nWhite participants proportion does not change in 2014 and 2020 in SP")
-    hyp_test(390, 145, 1465, 501)
-
-    print("\nHispano participants proportion does not change in 2014 and 2020 in SP")
-    hyp_test(390, 33, 1465, 137)
-
-    print("\nAsian participants proportion does not change in 2014 and 2020 in SP")
-    hyp_test(390, 190, 1465, 736)
-
-    print("\nBlack participants proportion does not change in 2014 and 2020 in SP")
-    hyp_test(390, 22, 1465, 91)
+    print("\nBlack participants proportion does not change in 2016 and 2019 in CCS")
+    hyp_test(1310, 77, 1543, 58)
 
     print('\n', '*'*70)
 
     print('\n')
-    print("Female proportion does not change in 2016 and 2020 in EuroSP")
-    hyp_test(216, 31, 289, 74)
+    print("Female proportion does not change in 2014 and 2019 in SP")
+    hyp_test(390, 67, 1209, 278)
 
-    print("\nWhite participants proportion does not change in 2016 and 2020 in EuroSP")
-    hyp_test(216, 114, 289, 115)
+    print("\nWhite participants proportion does not change in 2014 and 2019 in SP")
+    hyp_test(390, 145, 1209, 417)
 
-    print("\nHispano participants proportion does not change in 2016 and 2020 in EuroSP")
-    hyp_test(216, 26, 289, 56)
+    print("\nHispano participants proportion does not change in 2014 and 2019 in SP")
+    hyp_test(390, 33, 1209, 119)
 
-    print("\nAsian participants proportion does not change in 2016 and 2020 in EuroSP")
-    hyp_test(216, 58, 289, 106)
+    print("\nAsian participants proportion does not change in 2014 and 2019 in SP")
+    hyp_test(390, 190, 1209, 616)
 
-    print("\nBlack participants proportion does not change in 2016 and 2020 in EuroSP")
-    hyp_test(216, 18, 289, 12)
+    print("\nBlack participants proportion does not change in 2014 and 2019 in SP")
+    hyp_test(390, 22, 1209, 57)
+
+    print('\n', '*'*70)
+
+    print('\n')
+    print("Female proportion does not change in 2016 and 2019 in EuroSP")
+    hyp_test(216, 31, 418, 76)
+
+    print("\nWhite participants proportion does not change in 2016 and 2019 in EuroSP")
+    hyp_test(216, 114, 418, 155)
+
+    print("\nHispano participants proportion does not change in 2016 and 2019 in EuroSP")
+    hyp_test(216, 26, 418, 66)
+
+    print("\nAsian participants proportion does not change in 2016 and 2019 in EuroSP")
+    hyp_test(216, 58, 418, 169)
+
+    print("\nBlack participants proportion does not change in 2016 and 2019 in EuroSP")
+    hyp_test(216, 18, 418, 28)
 
     print('\n', '*'*70)
     print('\nCompare two different conferences')
     print('\n', '*'*70)
 
     print('\n')
-    print("Female proportion does not change between EuroSP and CCS in 2020")
-    hyp_test(289, 74, 175, 33)
+    print("Female proportion does not change between EuroSP and CCS in 2019")
+    hyp_test(418, 76, 1543, 313)
 
-    print("\nWhite participants proportion does not change between EuroSP and CCS in 2020")
-    hyp_test(289, 115, 175, 56)
+    print("\nWhite participants proportion does not change between EuroSP and CCS in 2019")
+    hyp_test(418, 115, 1543, 513)
 
-    print("\nHispano participants proportion does not change between EuroSP and CCS in 2020")
-    hyp_test(289, 56, 175, 24)
+    print("\nHispano participants proportion does not change between EuroSP and CCS in 2019")
+    hyp_test(418, 66, 1543, 149)
 
-    print("\nAsian participants proportion does not change between EuroSP and CCS in 2020")
-    hyp_test(289, 106, 175, 82)
+    print("\nAsian participants proportion does not change between EuroSP and CCS in 2019")
+    hyp_test(418, 169, 1543, 823)
 
-    print("\nBlack participants proportion does not change between EuroSP and CCS in 2020")
-    hyp_test(289, 12, 175, 13)
+    print("\nBlack participants proportion does not change between EuroSP and CCS in 2019")
+    hyp_test(418, 28, 1543, 58)
 
-
-    print('\n', '*'*70)
-
-    print('\n')
-    print("Female proportion does not change between SP and CCS in 2020")
-    hyp_test(1465, 274, 175, 33)
-
-    print("\nWhite participants proportion does not change between SP and CCS in 2020")
-    hyp_test(1465, 501, 175, 56)
-
-    print("\nHispano participants proportion does not change between SP and CCS in 2020")
-    hyp_test(1465, 137, 175, 24)
-
-    print("\nAsian participants proportion does not change between SP and CCS in 2020")
-    hyp_test(1465, 736, 175, 82)
-
-    print("\nBlack participants proportion does not change between SP and CCS in 2020")
-    hyp_test(1465, 91, 175, 13)
 
     print('\n', '*'*70)
 
     print('\n')
-    print("Female proportion does not change between SP and EuroSP in 2020")
-    hyp_test(1465, 274, 289, 74)
+    print("Female proportion does not change between SP and CCS in 2019")
+    hyp_test(1209, 278, 1543, 313)
 
-    print("\nWhite participants proportion does not change between SP and EuroSP in 2020")
-    hyp_test(1465, 501, 289, 115)
+    print("\nWhite participants proportion does not change between SP and CCS in 2019")
+    hyp_test(1209, 417, 1543, 513)
 
-    print("\nHispano participants proportion does not change between SP and EuroSP in 2020")
-    hyp_test(1465, 137, 289, 56)
+    print("\nHispano participants proportion does not change between SP and CCS in 2019")
+    hyp_test(1209, 119, 1543, 149)
 
-    print("\nAsian participants proportion does not change between SP and EuroSP in 2020")
-    hyp_test(1465, 736, 289, 106)
+    print("\nAsian participants proportion does not change between SP and CCS in 2019")
+    hyp_test(1209, 616, 1543, 823)
 
-    print("\nBlack participants proportion does not change between SP and EuroSP in 2020")
-    hyp_test(1465, 91, 289, 12)
+    print("\nBlack participants proportion does not change between SP and CCS in 2019")
+    hyp_test(1209, 57, 1543, 58)
+
+    print('\n', '*'*70)
+
+    print('\n')
+    print("Female proportion does not change between SP and EuroSP in 2019")
+    hyp_test(1209, 278, 418, 76)
+
+    print("\nWhite participants proportion does not change between SP and EuroSP in 2019")
+    hyp_test(1209, 417, 418, 155)
+
+    print("\nHispano participants proportion does not change between SP and EuroSP in 2019")
+    hyp_test(1209, 119, 418, 66)
+
+    print("\nAsian participants proportion does not change between SP and EuroSP in 2019")
+    hyp_test(1209, 616, 418, 169)
+
+    print("\nBlack participants proportion does not change between SP and EuroSP in 2019")
+    hyp_test(1209, 57, 418, 28)
